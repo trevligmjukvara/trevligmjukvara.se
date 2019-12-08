1f33bbd2773cfd2618336b20ace576c10705c08a
Sebastian Gelotte <sebastian.gelotte@gmail.com>
2019-12-08 18:10:22 +0100

    initial commit

---
+++ site/src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>trevligmjukvara.se</h1>
+    <p>Välkommen till vår podcast!</p>
+  </Layout>
+)
+
+export default IndexPage
