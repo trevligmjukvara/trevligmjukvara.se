d7eff6933073f9c403f7afe5a94dbb4927599c21
Sebastian Gelotte <sebastian.gelotte@gmail.com>
2019-12-11 18:17:43 +0100

    Adding sample episode

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -27,6 +27,20 @@ module.exports = {
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-page-creator",
+      options: {
+        path: `${__dirname}/src/episodes`,
+      },
+    },
+    `gatsby-plugin-mdx`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
