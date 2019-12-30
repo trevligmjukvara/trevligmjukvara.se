3f5cd412e14ab3722df171c8ff8df361b54439e5
Sebastian Gelotte <sebastian.gelotte@gmail.com>
2019-12-30 14:30:55 +0100

    Subscribe buttons added for various services

--- site/src/components/header.js
+++ site/src/components/header.js
@@ -2,7 +2,15 @@ import { Link } from "gatsby"
 import React from "react"
 import styled from "styled-components"
 import logo from "../images/logo.png"
-import { FaEnvelope, FaTwitter, FaRss } from "react-icons/fa"
+import {
+  FaEnvelope,
+  FaTwitter,
+  FaRss,
+  FaSpotify,
+  FaItunesNote,
+  FaGoogle,
+  FaPodcast,
+} from "react-icons/fa"
 
 const Header = ({ siteTitle }) => (
   <>
@@ -37,12 +45,54 @@ const Header = ({ siteTitle }) => (
       </BoxWrapper>
     </Wrapper>
     <ButtonsWrapper>
-      <RssButton
+      <StyledButton
+        href="https://podcasts.apple.com/se/podcast/trevlig-mjukvara/id1491691904"
+        target="_blank"
+        backgroundColor="#b250e2"
+        borderColor="#722996"
+      >
+        <FaItunesNote /> iTunes
+      </StyledButton>
+      <StyledButton
+        href="https://podcasts.google.com/?feed=aHR0cHM6Ly9mZWVkcy5maXJlc2lkZS5mbS90cmV2bGlnbWp1a3ZhcmEvcnNz"
+        target="_blank"
+        backgroundColor="#4285F4"
+        borderColor="#2c62bd"
+      >
+        <FaGoogle /> Google Podcasts
+      </StyledButton>
+      <StyledButton
+        href="https://open.spotify.com/show/31BqdspWOoQQVKSIfRlaox?si=Ita6dqtjSWGtUZ3U2y27xA"
+        target="_blank"
+        backgroundColor="#1DB954"
+        borderColor="#128d3e"
+      >
+        <FaSpotify /> Spotify
+      </StyledButton>
+      <StyledButton
+        href="https://pca.st/t5v44fi7"
+        target="_blank"
+        backgroundColor="#f43e37"
+        borderColor="#ad1510"
+      >
+        <FaPodcast /> PocketCasts
+      </StyledButton>
+      <StyledButton
+        href="https://www.stitcher.com/podcast/trevlig-mjukvara"
+        target="_blank"
+        backgroundColor="#622268"
+        borderColor="#4d1a52"
+      >
+        <FaPodcast /> Stitcher
+      </StyledButton>
+      <StyledButton
         href="https://feeds.fireside.fm/trevligmjukvara/rss"
         target="_blank"
+        backgroundColor="#ef8733"
+        borderColor="#bf6319"
       >
-        <FaRss /> Prenumerera
-      </RssButton>
+        <FaRss /> RSS
+      </StyledButton>
     </ButtonsWrapper>
   </>
 )
@@ -61,23 +111,39 @@ const Wrapper = styled.div`
   }
 `
 
-const ButtonsWrapper = styled.div`
-  margin-bottom: 10px;
-`
-
-const RssButton = styled.a`
+const StyledButton = styled.a`
   display: inline-block;
-  padding: 10px 20px;
-  background-color: #ffd68b;
-  border: 1px solid #d48900;
+  padding: 8px 20px 8px 40px;
+  background-color: ${props => props.backgroundColor || "#00c2d4"};
+  border: 1px solid ${props => props.borderColor || "#008d9a"};
   border-radius: 3px;
   cursor: pointer;
-  transition: background-color 0.05s ease-in;
-  font-size: 14px;
+  transition: opacity 0.05s ease-in;
+  font-size: 16px;
   text-decoration: none;
+  color: #fff;
+  position: relative;
 
   &:hover {
-    background-color: #ffb01f;
+    opacity: 0.8;
+  }
+
+  svg {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    left: 15px;
+  }
+`
+
+const ButtonsWrapper = styled.div`
+  ${StyledButton} {
+    margin-right: 10px;
+    margin-bottom: 10px;
+  }
+
+  @media only screen and (max-width: 800px) {
+    text-align: center;
   }
 `
 
