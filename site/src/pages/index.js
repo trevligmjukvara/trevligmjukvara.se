b8176099aeacc50798e0972082997cef8fa49463
Alexander Lindberg <alexanderlndbrg@gmail.com>
2020-02-03 00:45:32 +0100

    Fix player

--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -5,7 +5,7 @@ import LatestEpisode from "../episodes/s02e05.mdx"
 const IndexPage = () => {
   return (
     <>
-      <SEO title="Trevlig mjukvara" />
+      <SEO title="Trevlig Mjukvara" />
       <LatestEpisode />
     </>
   )
