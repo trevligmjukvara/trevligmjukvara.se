8905c568236706e3850ee341ffe5a44806fa4409
Alexander Lindberg <alexanderlndbrg@gmail.com>
2020-02-03 00:26:57 +0100

    Fix landing hack

--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import SEO from "../components/seo"
-import LatestEpisode from "../episodes/s02e04.mdx"
+import LatestEpisode from "../episodes/s02e05.mdx"
 
 const IndexPage = () => {
   return (
