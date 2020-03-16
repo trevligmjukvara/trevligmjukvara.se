c6d705eebeaa93981f3a3db928494dffa8320a32
Alexander Lindberg <alexanderlndbrg@gmail.com>
2020-03-16 23:09:52 +0100

    Fix player

--- site/src/components/player.js
+++ site/src/components/player.js
@@ -35,7 +35,7 @@ const Player = ({ src }) => {
   }
 
   useEffect(() => {
-    setPlayerWrapper(document.getElementById("playerWrapper"))
+    setPlayerWrapper(window && document.getElementById("playerWrapper"))
   }, [])
 
   console.log(playerWrapper)
