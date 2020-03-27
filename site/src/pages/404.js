b3f5087d638bd637ff616dd948bcb9d6f4ee29e7
Sebastian Gelotte <sebastian.gelotte@gmail.com>
2020-03-27 09:11:31 +0100

    Wrap icon in span for accessibility

--- site/src/pages/404.js
+++ site/src/pages/404.js
@@ -6,7 +6,12 @@ const NotFoundPage = () => (
   <>
     <SEO title="404: Vad söker du?" />
     <h1>Vi hittar inte vad du söker</h1>
-    <p>Lyssna på ett annat avsnitt istället? 😍</p>
+    <p>
+      Lyssna på ett annat avsnitt istället?
+      <span role="img" aria-label="icon">
+        😍
+      </span>
+    </p>
   </>
 )
 
